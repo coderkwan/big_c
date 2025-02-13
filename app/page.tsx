a6f907615b74fa6d2affd3a7f11fc06c93bfd578
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2025-02-13 10:44:50 +0200

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,171 @@
 "use client";
-import { useEffect, useState } from "react";
+
+import {useEffect, useState} from "react";
+import Image from "next/image";
+
 export default function Home() {
-        const titles = [
-                "Premium Workwear, Safety Gear & Corporate Uniforms – Designed for Durability & Style",
-                "Custom Branding & Printing: DTF, Sublimation, UV & Vinyl for Apparel, Cars & More!",
-                "From Fabrics to Final Product – Expert Manufacturers of T-Shirts, Uniforms & 3D Cutouts",
-        ];
-        const images = [
-                "1.png",
-                "2.png",
-                "3.png",
-                "4.png",
-                "5.png",
-                "6.png",
-                "7.png",
-                "8.png",
-        ];
+    const titles = [
+        "Signage Printing, Stockiest of signage materials",
+        "Custom Branding & Printing: DTF, Sublimation, UV & Vinyl for Apparel",
+        "Vihicle branding, Banners and Promotional Items",
+        "Premium Workwear, Safety Gear & Corporate Uniforms",
+    ];
+    const images = [
+        {
+            name: "Branding",
+            image: "1.png"
+        },
+        {
+            name: "Graphic design",
+            image: "2.png"
+        },
+        {
+            name: "Signage Materials",
+            image: "3.png"
+        },
+        {
+            name: "Signage Printing",
+            image: "4.png"
+        },
+        {
+            name: "Road Signs",
+            image: "5.png"
+        },
+        {
+            name: "DTF",
+            image: "6.png"
+        },
+        {
+            name: "Sublimation",
+            image: "7.png"
+        },
+    ];
 
-        const [count, setCount] = useState(0);
-        const [count_img, setCount_img] = useState(0);
+    const [count, setCount] = useState(0);
+    const [count_img, setCount_img] = useState(0);
 
-        useEffect(() => {
-                const interval = setInterval(() => {
-                        setCount((count) => (count + 1) % titles.length);
-                }, 6000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((count) => (count + 1) % titles.length);
+        }, 6000);
 
-                const interval_images = setInterval(() => {
-                        setCount_img(
-                                (count_img) => (count_img + 1) % images.length
-                        );
-                }, 3000);
-                return () => {
-                        clearInterval(interval);
-                        clearInterval(interval_images);
-                };
-        }, []);
+        const interval_images = setInterval(() => {
+            setCount_img((count_img) => (count_img + 1) % images.length);
+        }, 3000);
+        return () => {
+            clearInterval(interval);
+            clearInterval(interval_images);
+        };
+    }, []);
 
-        return (
-                <div className="flex flex-col md:flex-row gap-[4rem justify-between gap-5 items-center my-[5rem]">
-                        <div className=" md:w-[50%] flex flex-col gap-4">
-                                <h1 className="text-3xl md:text-4xl font-bold">
-                                        {titles[count]}
-                                </h1>
-                                <p className="text-sm text-slate-700">
-                                        At Big C Printworks, we specialize in
-                                        high-quality workwear, safety wear,
-                                        corporate uniforms, and custom branding
-                                        solutions. From security uniforms and
-                                        PPE to DTF, sublimation, and vinyl
-                                        printing, we provide top-tier products
-                                        tailored to your needs. Whether you are
-                                        a business looking for professional
-                                        uniforms or need custom branding for
-                                        cars, fabrics, and promotional
-                                        materials, we got you covered.
-                                </p>
-                                <div className="flex gap-4">
-                                        <button className="bg-black text-slate-200 py-3 px-9 ">
-                                                Services
-                                        </button>
-                                        <button className="bg-yellow-500 text-slate-900 py-3 px-9 ">
-                                                Contact Us
-                                        </button>
-                                </div>
-                        </div>
-                        <div className=" w-full  md:w-[50%]  flex items-start justify-start md:justify-center">
-                                <img
-                                        className="w-full md:w-auto md:max-h-[400px] "
-                                        src={`/hero/${images[count_img]}`}
-                                        alt="printing"
-                                />
-                        </div>
+    return (
+        <main>
+            <div className="mb-[10rem] flex flex-col md:flex-row gap-[4rem justify-between gap-5 items-center my-[5rem]">
+                <div className=" md:w-[50%] flex flex-col gap-4">
+                    <h1 className="text-3xl md:text-5xl font-bold">{titles[count]}</h1>
+                    <p className="text-sm md:text-[17px] leading-[22px] text-slate-700">
+                        Big C Printworks specializas in signage printing and fabrication, stockiest
+                        of signage material, Work wear, Corporate wear, Security Uniforms,
+                        Sublimation, DTF, PPE, Safety wear, 3D cutouts, UV printing, Vinyl Printing
+                        & Cutting, Car branding, Fabrics and T-shirts Manufacturers, Aluminum,
+                        Steel, We provide a lasting solution to all your branding needs.
+                    </p>
+                    <div className="flex gap-4">
+                        <button className="bg-black text-slate-200 py-3 px-9 ">Services</button>
+                        <button className="bg-orange-500 text-slate-100 py-3 px-9 ">
+                            Contact Us
+                        </button>
+                    </div>
                 </div>
-        );
+                <div className="w-full md:w-[50%] flex flex-col items-center justify-start md:justify-center">
+                    <img
+                        className="w-full md:w-auto md:max-h-[400px] "
+                        src={`/hero/${images[count_img]["image"]}`}
+                        alt="printing"
+                    />
+                    <p className="bg-slate-900 text-slate-100 font-bold p-3 uppercase w-full text-center">{images[count_img]["name"]}</p>
+                </div>
+            </div>
+            <section className="my-[10rem] flex flex-col gap-4 items-center">
+                <div className="text-center">
+                    <h2 className="text-4xl font-bold uppercase">What we do</h2>
+                    <p>We do stuff you'll never imagine</p>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                    <div className="flex flex-col px-6 pb-6 shadow-lg border border-slate-300 gap-5 ">
+                        <div className="relative w-full h-[300px]">
+                            <Image src="/hero/2.png" alt="signage printing" objectFit="cover" layout="fill" />
+                        </div>
+                        <h3 className="text-xl text-orange-500 font-bold">Signage Printing and Fabrication</h3>
+                        <p>Custom stationery solutions tailored for your business.</p>
+                        <button className="bg-orange-500 text-slate-200  p-3">View deltails on Signage Printing</button>
+                    </div>
+                    <div className="flex flex-col px-6 pb-6 shadow-lg border border-slate-300 gap-5 ">
+                        <div className="relative w-full h-[300px]">
+                            <Image src="/hero/2.png" alt="signage printing" objectFit="cover" layout="fill" />
+                        </div>
+                        <h3 className="text-xl text-black font-bold">Signage Printing and Fabrication</h3>
+                        <p>Custom stationery solutions tailored for your business.</p>
+                        <button className="bg-black text-slate-200  p-3">View deltails on Signage Printing</button>
+                    </div><div className="flex flex-col px-6 pb-6 shadow-lg border border-slate-300 gap-5">
+                        <div className="relative w-full h-[300px]">
+                            <Image src="/hero/2.png" alt="signage printing" objectFit="cover" layout="fill" />
+                        </div>
+                        <h3 className="text-xl text-orange-500 font-bold">Signage Printing and Fabrication</h3>
+                        <p>Custom stationery solutions tailored for your business.</p>
+                        <button className="bg-orange-500 text-slate-200  p-3">View deltails on Signage Printing</button>
+                    </div><div className="flex flex-col px-6 pb-6 shadow-lg border border-slate-300 gap-5">
+                        <div className="relative w-full h-[300px]">
+                            <Image src="/hero/2.png" alt="signage printing" objectFit="cover" layout="fill" />
+                        </div>
+                        <h3 className="text-xl text-black font-bold">Signage Printing and Fabrication</h3>
+                        <p>Custom stationery solutions tailored for your business.</p>
+                        <button className="bg-black text-slate-200  p-3">View deltails on Signage Printing</button>
+                    </div><div className="flex flex-col px-6 pb-6 shadow-lg border border-slate-300 gap-5">
+                        <div className="relative w-full h-[300px]">
+                            <Image src="/hero/2.png" alt="signage printing" objectFit="cover" layout="fill" />
+                        </div>
+                        <h3 className="text-xl text-orange-500 font-bold">Signage Printing and Fabrication</h3>
+                        <p>Custom stationery solutions tailored for your business.</p>
+                        <button className="bg-orange-500 text-slate-200  p-3">View deltails on Signage Printing</button>
+                    </div><div className="flex flex-col px-6 pb-6 shadow-lg border border-slate-300 gap-5">
+                        <div className="relative w-full h-[300px]">
+                            <Image src="/hero/2.png" alt="signage printing" objectFit="cover" layout="fill" />
+                        </div>
+                        <h3 className="text-xl text-black font-bold">Signage Printing and Fabrication</h3>
+                        <p>Custom stationery solutions tailored for your business.</p>
+                        <button className="bg-black text-slate-200  p-3">View deltails on Signage Printing</button>
+                    </div>
+                </div>
+            </section>
+            <section className="my-[10rem] flex flex-col gap-4 items-center">
+                <div className="text-center">
+                    <h2 className="text-4xl font-bold uppercase">Why you should choose us?</h2>
+                    <p>The only reasons why you shoul work with Big C Printworks.</p>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 text-center gap-10">
+                    <div className="flex flex-col items-center gap-3">
+                        <Image src='/quality.png' alt="high quality" width={50} height={50} />
+                        <h3 className="uppercase text-xl">High-Quality Printing Services</h3>
+                        <p>Boost your business with Print To Brand. We offer top-quality prints, from business cards to banners, making your brand shine.</p>
+                    </div>
+                    <div className="flex flex-col  items-center gap-3">
+                        <Image src='/custom.png' alt="high quality" width={50} height={50} />
+                        <h3 className="uppercase text-xl">High-Quality Printing Services</h3>
+                        <p>Boost your business with Print To Brand. We offer top-quality prints, from business cards to banners, making your brand shine.</p>
+                    </div>
+                    <div className="flex flex-col  items-center gap-3">
+                        <Image src='/expertise.png' alt="high quality" width={50} height={50} />
+                        <h3 className="uppercase text-xl">High-Quality Printing Services</h3>
+                        <p>Boost your business with Print To Brand. We offer top-quality prints, from business cards to banners, making your brand shine.</p>
+                    </div>
+                    <div className="flex flex-col  items-center gap-3">
+                        <Image src='/speed.png' alt="high quality" width={50} height={50} />
+                        <h3 className="uppercase text-xl">High-Quality Printing Services</h3>
+                        <p>Boost your business with Print To Brand. We offer top-quality prints, from business cards to banners, making your brand shine.</p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,31 +2,36 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Nav() {
-        return (
-                <div className="bg-black text-slate-100 px-3">
-                        <div className="mx-auto max-w-[1200px] flex items-center justify-between gap-4">
-                                <div className="p-2">
-                                        <Image
-                                                src="/weblogo.png"
-                                                alt="big c logo"
-                                                width={120}
-                                                height={40}
-                                        />
-                                </div>
-                                <div className="gap-5 hidden md:flex">
-                                        <Link href="services">Services</Link>
-                                        <Link href="products">Products</Link>
-                                        <Link href="about">About Us</Link>
-                                        <Link href="contact">Contact Us</Link>
-                                </div>
-                                <Image
-                                        src="/menu.png"
-                                        alt="menu"
-                                        width={40}
-                                        height={40}
-                                        className="md:hidden"
-                                />
-                        </div>
+    return (
+        <div className="bg-black text-slate-100 p-3">
+            <div className="mx-auto max-w-[1200px] flex items-center justify-between gap-4">
+                <div className="p-2">
+                    <Image
+                        src="/weblogo.png"
+                        alt="big c logo"
+                        width={120}
+                        height={40}
+                    />
                 </div>
-        );
+                <div className="gap-10 hidden md:flex items-center">
+                    <Link href="services">Services</Link>
+                    <Link href="products">Products</Link>
+                    <Link href="about">About Us</Link>
+                    <Link href="contact">Contact Us</Link>
+                    <Link href="https://" className="bg-white py-2 px-5 rounded-md text-slate-800">
+                        <div className="flex gap-2">
+                            <Image className="w-[25%]" src='/wp.png' alt="whatsapp icon" width={20} height={20} /><p>WhatsApp</p></div>
+                    </Link>
+
+                </div>
+                <Image
+                    src="/menu.png"
+                    alt="menu"
+                    width={40}
+                    height={40}
+                    className="md:hidden"
+                />
+            </div>
+        </div>
+    );
 }
